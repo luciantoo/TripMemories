01d75a481e19160554a57cd6487ad7296e39029d
Denis Buliga <denis.buliga@audionowdigital.com>
2014-04-11 20:02:56 +0300

    Take a picture!

--- app/src/main/java/itec/code2smile/MainActivity.java
+++ app/src/main/java/itec/code2smile/MainActivity.java
@@ -1,28 +1,71 @@
 package itec.code2smile;
 
 import android.app.Activity;
-import android.app.ActionBar;
 import android.app.Fragment;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 
 public class MainActivity extends Activity {
 
+    private final static int ACTIVITY_RESULT = 2;
+    private Uri imageUri;
+    private ImageView img;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
 
-        if (savedInstanceState == null) {
-            getFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
-                    .commit();
-        }
+        LinearLayout ll = new LinearLayout(this);
+        ll.setOrientation(LinearLayout.VERTICAL);
+
+        Button take_btn = new Button(this);
+        take_btn.setText("Take a picture!");
+        take_btn.setClickable(true);
+        take_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Log.d("onClick","s-a apasat");
+
+                Intent mIntent = new Intent("android.media.action.IMAGE_CAPTURE");
+
+                String state = Environment.getExternalStorageState();
+                if (Environment.MEDIA_MOUNTED.equals(state))
+                {
+                    Log.d("State",state);
+                    File photoFile = new File(Environment.getExternalStorageDirectory(),"photo.jpg");
+                    mIntent.putExtra(MediaStore.EXTRA_OUTPUT,Uri.fromFile(photoFile));
+                    imageUri = Uri.fromFile(photoFile);
+                }
+
+                startActivityForResult(mIntent,ACTIVITY_RESULT);
+            }
+        });
+
+        img = new ImageView(this);
+
+        ll.addView(take_btn);
+        ll.addView(img);
+        setContentView(ll);
     }
 
 
@@ -46,20 +89,36 @@ public class MainActivity extends Activity {
         return super.onOptionsItemSelected(item);
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
 
-        public PlaceholderFragment() {
-        }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==ACTIVITY_RESULT)
+            if(resultCode==RESULT_OK)
+            {
+                Uri selectedImageUri = imageUri;
 
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            return rootView;
-        }
+                try {
+
+                    //ImageView img = (ImageView)findViewById(R.id.image_view);
+
+                    InputStream inputStream = getContentResolver().openInputStream(selectedImageUri);
+                    BitmapFactory.Options options = new BitmapFactory.Options();
+                    options.inSampleSize = 8;
+                    Bitmap myBitmap = BitmapFactory.decodeStream(inputStream, null, options);
+                    img.setImageBitmap(myBitmap);
+
+                } catch (NullPointerException e) {
+                    e.printStackTrace();
+                    Log.d("NullPointerException",""+e.getMessage());
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                    Log.d("FileNotFoundException",""+e.getMessage());
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    Log.d("Exception",""+e.getMessage());
+                }
+            }
     }
 
 }
